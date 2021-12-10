91d87190c959d4f6f206674f9f5cd6d9cfdac2ce
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-12-10 20:06:37 -0300

    created database.go and the code for instantiate the database

---
+++ src/database/database.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func ConnectDb() {
+	var dsn = "root:mysql0401@/go_api?charset=utf8mb4&parseTime=True&loc=Local"
+	var v = "Não conseguiu conectar ao banco de dados\n"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(v, err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Conectado ao banco de dados!")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Executando as migrações")
+
+	Database = DbInstance{Db: db}
+}
