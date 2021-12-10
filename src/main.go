31f74d87a79f6bb133194dd7b55eb099a8f1cd07
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-12-10 19:49:46 -0300

    first

--- src/main.go
+++ src/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"log"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to my awesome API")
+}
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/api", welcome)
 
-	log.Fatal(app.Listen(":300"))
+	log.Fatal(app.Listen(":3000"))
 }
