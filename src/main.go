7f62ab598509d54b536562a09a929307a8b940b5
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-12-11 14:04:07 -0300

    fix user.go and the call app.Post in main.go

--- src/main.go
+++ src/main.go
@@ -17,18 +17,6 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api", welcome)
 	// User endpoints
 	app.Post("/api/users", routes.CreateUser)
-	app.Get("/api/users", routes.GetUsers)
-	app.Get("/api/users/:id", routes.GetUser)
-	app.Delete("/api/users/:id", routes.DeleteUser)
-	// Product endpoints
-	app.Post("/api/products", routes.CreateProduct)
-	app.Get("/api/products", routes.GetProducts)
-	app.Get("/api/products/:id", routes.GetProduct)
-	app.Put("/api/products/:id", routes.UpdateProduct)
-	// Order endpoints
-	app.Post("/api/orders", routes.CreateOrder)
-	app.Get("/api/orders", routes.GetOrders)
-	app.Get("/api/orders/:id", routes.GetOrder)
 }
 
 func main() {
